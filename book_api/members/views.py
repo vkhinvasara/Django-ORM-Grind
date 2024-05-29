87a310a9ba6ce034047fafb7f49ad4539a221ffd
vkhinvasara <vaibhavkhinvasara@gmail.com>
2024-05-29 17:31:49 +0530

    lets do this again lol

--- book_api/members/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .serializers import BookSerializer
-from .models import Books
-
-class BookViewSet(viewsets.ModelViewSet):
-	queryset = Books.objects.all()
-	serializer_class = BookSerializer
-
